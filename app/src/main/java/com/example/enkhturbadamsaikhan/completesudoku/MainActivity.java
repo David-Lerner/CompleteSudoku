31ae51213f7d66ed999091d78d00004539281f5a
enkhturbadamsaikhan <enkhtursjsu@gmail.com>
2016-11-18 15:05:31 -0800

    added toolbar(settings and logout button )

--- app/src/main/java/com/example/enkhturbadamsaikhan/completesudoku/MainActivity.java
+++ app/src/main/java/com/example/enkhturbadamsaikhan/completesudoku/MainActivity.java
@@ -2,31 +2,76 @@ package com.example.enkhturbadamsaikhan.completesudoku;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.parse.ParseUser;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button logout;
+//    Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        logout = (Button) findViewById(R.id.b_logout_main);
-        logout.setOnClickListener(new View.OnClickListener() {
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Define the listener
+        MenuItemCompat.OnActionExpandListener expandListener = new MenuItemCompat.OnActionExpandListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                // Do something when action item collapses
+                return true;  // Return true to collapse action view
+            }
+
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                // Do something when expanded
+                return true;  // Return true to expand action view
+            }
+        };
+
+//        MenuItem searchItem = menu.findItem(R.id.);
+//        SearchView searchView =
+//                (SearchView) MenuItemCompat.getActionView(searchItem);
+//
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.logout_user:
                 ParseUser.logOut();
                 Intent i = new Intent(MainActivity.this, IntroActivity.class);
                 startActivity(i);
                 finish();
-            }
-        });
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
+
+
 }
